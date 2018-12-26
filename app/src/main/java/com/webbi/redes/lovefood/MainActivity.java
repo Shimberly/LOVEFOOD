28a8eba3dd042779adeead1e4685a9ac19508b65
Shimberly <kimmy_11_07@hotmail.com>
2018-12-25 21:15:07 -0500

    Pantalla Inicial / Login

--- app/src/main/java/com/webbi/redes/lovefood/MainActivity.java
+++ app/src/main/java/com/webbi/redes/lovefood/MainActivity.java
@@ -1,8 +1,10 @@
 package com.webbi.redes.lovefood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button IrARegistroBtn = (Button) findViewById(R.id.btnRegistro);
+        IrARegistroBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent itemintent = new Intent(MainActivity.this, Principal.class);
+                MainActivity.this.startActivity(itemintent);
+            }
+        });
     }
 }
