3d8a672b05d944c98b6a89fc372d711dcb1ce2f2
Shimberly <kimmy_11_07@hotmail.com>
2019-01-25 17:23:57 -0500

    Login con servicio rest

---
+++ app/src/main/java/com/webbi/redes/lovefood/ProblemaConexion.java
@@ -0,0 +1,40 @@
+package com.webbi.redes.lovefood;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class ProblemaConexion extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        String titulo = getArguments().getString("titulo");
+        String texto = getArguments().getString("texto");
+        return createSimpleDialog(titulo,texto);
+    }
+
+
+    public AlertDialog createSimpleDialog(String titulo, String texto) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(),R.style.CustomDialogTheme);
+
+        builder.setTitle(titulo)
+                .setMessage(texto)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Acciones
+                            }
+                        })
+                .setNegativeButton("CANCELAR",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Acciones
+                            }
+                        });
+
+        return builder.create();
+    }
+}
