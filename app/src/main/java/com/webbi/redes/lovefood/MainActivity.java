96260f8b24813ba89a5d91c17b9118dbfdc06705
Shimberly <kimmy_11_07@hotmail.com>
2019-01-25 19:28:13 -0500

    Cambio interfaz registro

--- app/src/main/java/com/webbi/redes/lovefood/MainActivity.java
+++ app/src/main/java/com/webbi/redes/lovefood/MainActivity.java
@@ -5,8 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+//import com.facebook.FacebookSdk;
+//import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+        //FacebookSdk.sdkInitialize(getApplicationContext());
+        //AppEventsLogger.activateApp(this);
 
         ImageView IrALogin = (ImageView) findViewById(R.id.irALogin);
         IrALogin.setOnClickListener(new View.OnClickListener() {
@@ -30,7 +30,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void printKeyHash(){
+       /* <activity
+        android:name="com.facebook.FacebookActivity"
+        android:configChanges= "keyboard|keyboardHidden|screenLayout|screenSize|orientation"
+        android:label="@string/app_name" />
 
+        <activity
+        android:name="com.facebook.CustomTabActivity"
+        android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.VIEW" />
+                <category android:name="android.intent.category.DEFAULT" />
+                <category android:name="android.intent.category.BROWSABLE" />
+                <data android:scheme="@string/fb_login_protocol_scheme" />
+            </intent-filter>
+        </activity>*/
     }
 
 }
--- app/src/main/java/com/webbi/redes/lovefood/Registrarse.java
+++ app/src/main/java/com/webbi/redes/lovefood/Registrarse.java
@@ -23,14 +23,6 @@ public class Registrarse extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinSe);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.genero_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
 
         EditText etPlannedDate = (EditText) findViewById(R.id.txtDate);
         etPlannedDate.setOnClickListener(new View.OnClickListener() {
@@ -53,23 +45,6 @@ public class Registrarse extends AppCompatActivity {
             }
         });
 
-        Button IrAInicio2 = (Button) findViewById(R.id.btnRegistroFb);
-        IrAInicio2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent itemintent = new Intent(Registrarse.this, Principal.class);
-                Registrarse.this.startActivity(itemintent);
-            }
-        });
-
-        Button IrAInicio3 = (Button) findViewById(R.id.btnRegistroGoogle);
-        IrAInicio3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent itemintent = new Intent(Registrarse.this, Principal.class);
-                Registrarse.this.startActivity(itemintent);
-            }
-        });
     }
     private void showDatePickerDialog() {
 
